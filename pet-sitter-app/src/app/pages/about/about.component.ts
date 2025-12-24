19591829ea196d39ff35cbb956cd3327340ab4a0
stefand799 <dinustefan1996@yahoo.com>
2025-12-24 09:42:48 +0200

    Update About  Us page

--- pet-sitter-app/src/app/pages/about/about.component.ts
+++ pet-sitter-app/src/app/pages/about/about.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, TranslateModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
